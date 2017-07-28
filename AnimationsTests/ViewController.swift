c6f6283084dbee9cd444098057fc30310e5294b5
olegreksa <oleg@gr-development.com>
2017-07-28 10:48:36 +0300

    Initial Commit

---
+++ AnimationsTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationsTests
+//
+//  Created by Олег Рекша on 28.07.17.
+//  Copyright © 2017 Олег Рекша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
